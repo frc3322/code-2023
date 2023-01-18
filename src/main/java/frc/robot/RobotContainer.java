555fdba03872fdbcb538d6b872022658d38005c1
Simran Kheterpal <simrankheterpal11@gmail.com>
2023-01-18 10:20:09 -0500

    Copied in Wapur drivetrain code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Drivetrain;
+import io.github.oblarg.oblog.Logger;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,14 +21,25 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+ 
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Drivetrain drivetrain = new Drivetrain();
+  
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController driverController = new CommandXboxController(0);
 
+    private final Command driveCommand = new RunCommand(
+      () -> {
+        double speed = MathUtil.applyDeadband(driverController.getLeftY(), 0.09);
+        double turn = MathUtil.applyDeadband(driverController.getRightX(), 0.08);
+        drivetrain.drive(speed, turn);
+      }
+      , drivetrain);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    Logger.configureLoggingAndConfig(this, false);
+
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +54,13 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  
+    
+   
+  }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  public void updateLogger(){
+    Logger.updateEntries();
   }
 
   /**
@@ -58,6 +70,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
+
 }
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,127 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableValue;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Log;
+
+public class Drivetrain extends SubsystemBase implements Loggable {
+  public final CANSparkMax motorFR = new CANSparkMax(Constants.CAN.FR, MotorType.kBrushless);
+  public final CANSparkMax motorFL = new CANSparkMax(Constants.CAN.FL, MotorType.kBrushless);
+  public final CANSparkMax motorBR = new CANSparkMax(Constants.CAN.BR, MotorType.kBrushless);
+  public final CANSparkMax motorBL = new CANSparkMax(Constants.CAN.BL, MotorType.kBrushless);
+  
+  private final DifferentialDrive robotDrive = new DifferentialDrive(motorFL, motorFR);
+  
+  private final SlewRateLimiter accelLimit = new SlewRateLimiter(1.2);
+  private final SlewRateLimiter turnLimit = new SlewRateLimiter(2);
+
+  //gets the default instance of NetworkTables that is automatically created
+  NetworkTableInstance inst = NetworkTableInstance.getDefault();
+  
+  //gets the limelight table where data is stored from the limelight
+  NetworkTable limelightTable = inst.getTable("limelight");
+
+
+  // Create gyro
+  // private final AHRS gyro = new AHRS();
+
+  // Create double for logging the yaw of the robot
+  // @Log private double heading = -999;
+
+  // create double for logging the controller input
+  @Log private double speed = -2;
+  @Log private double turn = -2;
+
+  /** Creates a new ExampleSubsystem. */
+  public Drivetrain() {
+    motorFL.setInverted(true);
+    motorFR.setInverted(false);
+    motorBR.follow(motorFR);
+    motorBL.follow(motorFL);
+
+
+    motorFR.setIdleMode(IdleMode.kBrake);
+    motorFL.setIdleMode(IdleMode.kBrake);
+    motorBR.setIdleMode(IdleMode.kBrake);
+    motorBL.setIdleMode(IdleMode.kBrake);
+
+    motorFR.burnFlash();
+    motorFL.burnFlash();
+    motorBR.burnFlash();
+    motorBL.burnFlash();
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  public void drive(double speed, double turn) {
+    //turn = 0.5 * turn + 0.5 * Math.pow(turn, 3);  // Weird math
+
+    this.speed = speed;
+    this.turn = turn;
+
+    robotDrive.arcadeDrive(accelLimit.calculate(speed), turnLimit.calculate(turn), false);
+
+    robotDrive.feed();
+  }
+
+  // Limelight Functions Start
+
+  public void setPipeline(int pipelineNum){
+    limelightTable
+      //gets the "pipeline" entry from the limelight table
+      .getEntry("pipeline")
+      //sets the value of the pipeline entry to the parameter of the function
+      .setNumber(pipelineNum);
+  }
+
+  // Limelight Functions End
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // heading = getHeading();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  // public double getHeading() {
+  //   return gyro.getRotation2d().getDegrees();
+  // }
+}
