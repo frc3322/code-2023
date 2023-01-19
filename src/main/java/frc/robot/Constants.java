4dfd8b98ffaf8295183b27836e812f6e1d6a7c87
Sean <you@example.com>
2023-01-18 20:38:45 -0500

    Merged Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,6 +12,9 @@ public final class Constants {
     public static final int FR = 3;
     public static final int BL = 4;
     public static final int BR = 11;
+    // Elevator
+    public static final int LElevator = 100;
+    public static final int RElevator = 101;
     //Intake Motors
     public static final int tRoller = 200;
     public static final int bRoller = 201;
