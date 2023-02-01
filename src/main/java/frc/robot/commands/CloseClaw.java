f5024cbeeffae0c9d099d5d3a5f18142969d3684
Caitlyn <cmtrievel@icloud.com>
2023-02-01 18:21:25 -0500

    added some skeleton to fourbar and claw commands

--- src/main/java/frc/robot/commands/CloseClaw.java
+++ src/main/java/frc/robot/commands/CloseClaw.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+import frc.robot.subsystems.Outtake;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 public class CloseClaw extends InstantCommand {
   public CloseClaw() {
     // Use addRequirements() here to declare subsystem dependencies.
+    //addRequirements(Outtake);
+    //Outtake.setClosed();
   }
 
   // Called when the command is initially scheduled.
