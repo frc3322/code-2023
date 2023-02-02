6928354303f9ca4e5c6125ed22ff34778bcd0d85
NumberEater <reddickgrant@gmail.com>
2023-02-01 19:01:31 -0500

    Filled IntakeGamePiece command

--- src/main/java/frc/robot/commands/IntakeGamePiece.java
+++ src/main/java/frc/robot/commands/IntakeGamePiece.java
@@ -2,21 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Uncomment Later
+package frc.robot.commands;
 
-// package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.robot.subsystems.Intake;
 
-// import edu.wpi.first.wpilibj2.command.StartEndCommand;
+public class IntakeGamePiece extends StartEndCommand {
+  public IntakeGamePiece(Intake intake) {
+    super(
+      () -> {
+        intake.spinIntake(0.1);
+      },
+      () -> {
+        intake.stopSpin();
+      },
+      intake);
+  }
 
-// public class IntakeGamePiece extends StartEndCommand {
-//   public IntakeGamePiece() {
-//     super(
-//       null,   // Extend intake arm and spin intake motors toward robot
-//       null,   // Retract intake arm and spit piece into transfer
-//       null);  // Uses intake subsystem
-//   }
-
-//   // Called when the command is initially scheduled.
-//   @Override
-//   public void initialize() {}
-// }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+}
