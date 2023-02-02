c1343720fec8fff31e9c0598ebefb5694ada1ac0
Caitlyn <cmtrievel@icloud.com>
2023-02-01 19:08:28 -0500

    added addRequirements

--- src/main/java/frc/robot/commands/CloseClaw.java
+++ src/main/java/frc/robot/commands/CloseClaw.java
@@ -5,11 +5,12 @@
 package frc.robot.commands;
 import frc.robot.subsystems.Outtake;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-
+import frc.robot.subsystems.Outtake;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class CloseClaw extends InstantCommand {
+  addRequirements(Outtake)
   public CloseClaw() {
     // Use addRequirements() here to declare subsystem dependencies.
     //addRequirements(Outtake);
