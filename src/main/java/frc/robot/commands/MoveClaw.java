d4f712be515461d728a1d94c30473b10fb036149
IBuildRoboats <313947@aaps.k12.mi.us>
2023-02-01 19:31:46 -0500

    Added constructor and enum for MoveClaw

--- src/main/java/frc/robot/commands/MoveClaw.java
+++ src/main/java/frc/robot/commands/MoveClaw.java
@@ -1,15 +1,24 @@
+package frc.robot.commands;
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package ;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Types.ClawPosition;
+import frc.robot.subsystems.Outtake;
+
+
 
 public class MoveClaw extends CommandBase {
+
+  private ClawPosition clawPosition;
+  private Outtake claw;
+
   /** Creates a new MoveClaw. */
-  public MoveClaw() {
+  public MoveClaw(ClawPosition clawPosition, Outtake claw) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.clawPosition = clawPosition;
+    this.claw = claw;
   }
 
   // Called when the command is initially scheduled.
