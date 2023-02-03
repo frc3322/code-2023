93e2f8d8641caf6583a8bdbe6c46a91583a7e583
Haley Dalzell <haleydalzell@gmail.com>
2023-02-02 22:39:57 -0500

    commented out subsystem/commands that require components that aren't on the robot yet

--- src/main/java/frc/robot/commands/MoveElevatorCommand.java
+++ src/main/java/frc/robot/commands/MoveElevatorCommand.java
@@ -1,45 +1,45 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
-import frc.robot.subsystems.Elevator;
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.wpilibj2.command.PIDCommand;
+// import frc.robot.subsystems.Elevator;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class MoveElevatorCommand extends PIDCommand {
+// // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// // information, see:
+// // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+// public class MoveElevatorCommand extends PIDCommand {
   
-  /** Creates a new MoveElevator. */
-  public MoveElevatorCommand(Elevator elevator, double targetPosition) {
+//   /** Creates a new MoveElevator. */
+//   public MoveElevatorCommand(Elevator elevator, double targetPosition) {
   
-    super(
+//     super(
       
-        // The controller that the command will use
-        new PIDController(0, 0, 0),
-        // This should return the measurement
-       elevator::getElevatorPosition,
-        // This should return the setpoint (can also be a constant)
-        targetPosition,
-        // This uses the output
-        output -> {
-          elevator.setPower(output);
-        });
+//         // The controller that the command will use
+//         new PIDController(0, 0, 0),
+//         // This should return the measurement
+//        elevator::getElevatorPosition,
+//         // This should return the setpoint (can also be a constant)
+//         targetPosition,
+//         // This uses the output
+//         output -> {
+//           elevator.setPower(output);
+//         });
 
-        addRequirements(elevator);
+//         addRequirements(elevator);
 
        
-    // Use addRequirements() here to declare subsystem dependencies.
-    // Configure additional PID options by calling `getController` here.
-  }
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     // Configure additional PID options by calling `getController` here.
+//   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return Math.abs(getController().getPositionError())<.2;
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return Math.abs(getController().getPositionError())<.2;
     
-  }
-}
+//   }
+// }
--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -1,39 +1,39 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//mport frc.robot.Elevator;
-public class Transfer extends SubsystemBase {
-  /** Creates a new Transfer. */
-  public static final CANSparkMax beltMotor = new CANSparkMax(Constants.CAN.T, MotorType.kBrushless);
-  public Transfer() {
-    beltMotor.setIdleMode(IdleMode.kBrake);
-    beltMotor.burnFlash();
-  }
-  //check if elevator is up or down. Could probably be a lambda in toElevator
-  public boolean getElevator() {
-    //Elevator.getPlace(); just a placholer for now
-    return false;
-  }
+// package frc.robot.subsystems;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMax.IdleMode;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import frc.robot.Constants;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// //mport frc.robot.Elevator;
+// public class Transfer extends SubsystemBase {
+//   /** Creates a new Transfer. */
+//   public static final CANSparkMax beltMotor = new CANSparkMax(Constants.CAN.T, MotorType.kBrushless);
+//   public Transfer() {
+//     beltMotor.setIdleMode(IdleMode.kBrake);
+//     beltMotor.burnFlash();
+//   }
+//   //check if elevator is up or down. Could probably be a lambda in toElevator
+//   public boolean getElevator() {
+//     //Elevator.getPlace(); just a placholer for now
+//     return false;
+//   }
  
-  public static void setBeltPower(double power){
-    beltMotor.set(power);
-  }
+//   public static void setBeltPower(double power){
+//     beltMotor.set(power);
+//   }
 
 
-  public void stop() {
-    beltMotor.stopMotor();
-  }
+//   public void stop() {
+//     beltMotor.stopMotor();
+//   }
 
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-}
+//   @Override
+//   public void periodic() {
+//     // This method will be called once per scheduler run
+//   }
+// }
