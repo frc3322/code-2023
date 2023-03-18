717e137ab6a8fffa628d6c9db5a65c07c623a41c
IBuildRoboats <313947@aaps.k12.mi.us>
2023-03-17 23:41:33 -0400

    Fixed exampleTrajectory to use X instead of Y

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -300,8 +300,8 @@ public class RobotContainer implements Loggable{
     Trajectory exampleTrajectory =
         TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, new Rotation2d(0)),
-            List.of(new Translation2d(0, 0.5)),
-            new Pose2d(0, 1, new Rotation2d(0)),
+            List.of(new Translation2d(0.5, 0)),
+            new Pose2d(1, 0, new Rotation2d(0)),
             AutonConstants.kDriveTrajectoryConfig);
 
     private StraightLine(){
